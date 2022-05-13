69ae64c76d23e3a648a3c7e8640916839d2bced2
JooVitorSena <jvhc3002@gmail.com>
2022-05-12 22:40:00 -0300

    Darkmode Implementado corretamente

--- js/index.js
+++ js/index.js
@@ -1,2 +1,11 @@
-function trocaDeTema() {
-}
+const input = document.querySelector('#checkbox')
+
+
+input.addEventListener('change', function () {
+  if(input.checked == true){
+    console.log('Funcionou?')
+    document.getElementById('tema').href = './stylesbase/styledark.css'
+  } else{
+    document.getElementById('tema').href = './stylesbase/style.css'
+  }
+})
