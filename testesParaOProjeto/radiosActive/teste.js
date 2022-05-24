bdb3820dcd1aa97fd77b5fb0bd08f3dd9f507159
JooVitorSena <jvhc3002@gmail.com>
2022-05-24 20:29:39 -0300

    Commit jv user page

--- testesParaOProjeto/radiosActive/teste.js
+++ testesParaOProjeto/radiosActive/teste.js
@@ -1,38 +1,23 @@
-const input = document.querySelector('#checkbox')
-
-
-input.addEventListener('change', function () {
-  if(input.checked == true){
-    document.getElementById('tema').href = './stylesbase/usuariodark.css'
-  } else{
-    document.getElementById('tema').href = './stylesbase/usuario.css'
-  }
-})
-
 const posts = document.getElementById("posts");
 const salvos = document.getElementById("salvos");
-const provas = document.getElementById("provas");
-const feed = document.getElementById("feed");
-var x = feed.scrollWidth;
-var xSalvo = x/3;
+const provas = document.getElementById("gabaritos");
+
 
 posts.addEventListener('click', function(){
   posts.classList.add("btnActive")
   salvos.classList.remove("btnActive")
   provas.classList.remove("btnActive")
-  feed.scrollTo(0, 0)
 })
 
 salvos.addEventListener('click', function(){
   salvos.classList.add("btnActive")
   posts.classList.remove("btnActive")
   provas.classList.remove("btnActive")
-  feed.scrollTo(xSalvo, 0)
 })
 
 provas.addEventListener('click', function(){
   provas.classList.add("btnActive")
   posts.classList.remove("btnActive")
   salvos.classList.remove("btnActive")
-  feed.scrollTo(x, 0)
 })
+
