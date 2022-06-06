471215956bb2f9519e7c0102c508bd2533b56616
Walmir-Ag <walmirfaguiar100@gmail.com>
2022-06-06 14:09:38 -0300

    ejs walmir

---
+++ express.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express();
+const port = 3000
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render('ejs/views/pages/cadastro-login')
+}) 
+
+app.listen(port, () =>{
+     console.log(`projeto rodando na port: ${port}`)
+})
