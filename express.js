3d270cdfd347ca35149105dfd1db78f8ec7f075f
Walmir-Ag <walmirfaguiar100@gmail.com>
2022-06-06 16:02:48 -0300

    comitando ejs-detalhes

--- express.js
+++ express.js
@@ -1,12 +1,36 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3007;
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"))
 
 app.get("/", (req, res) => {
     res.render('pages/index');
+});
+
+app.get("/cadastro-login", (req, res) => {
+    res.render('pages/cadastro-login');
+});
+
+app.get("/cadastro", (req, res) => {
+    res.render('pages/cadastro');
+});
+
+app.get("/erro", (req, res) => {
+    res.render('pages/Erro');
+});
+
+app.get("/login", (req, res) => {
+    res.render('pages/login');
+});
+
+app.get("/noticias", (req, res) => {
+    res.render('pages/noticias');
+});
+
+app.get("/usuario", (req, res) => {
+    res.render('pages/usuario');
 });
 
 app.listen(port, () =>{
