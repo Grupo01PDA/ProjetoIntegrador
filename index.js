11277e6671ade28fdd454ecf1f72dac20476ad52
Walmir-Ag <walmirfaguiar100@gmail.com>
2022-06-12 17:51:16 -0300

    variaveis para users

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 const app = express();
 const port = 3007;
@@ -5,16 +6,29 @@ const port = 3007;
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"))
 
+const allNewUser = () => {
+let contentPreference= ["Tecnology", "Enem", "História Brasileira", "Bestsellers", "línguas"]
+let users= [
+    {usuario:"Ana Laura", idade: 18, topPreferences: contentPreference, grauEscolar: "Ensino Médio completo"}
+]
+console.log(users)
+}
+
+function userLogin(){
+console.log[{usuario: "Gui", senha: "#PizzacomAmor"}]
+} 
+
 app.get("/", (req, res) => {
     res.render('pages/index');
 });
 
 app.get("/cadastro-login", (req, res) => {
     res.render('pages/cadastro-login');
-});
+}); 
 
 app.get("/cadastro", (req, res) => {
     res.render('pages/cadastro');
+    return response.json({allNewUser}) 
 });
 
 app.get("/erro", (req, res) => {
@@ -23,6 +37,7 @@ app.get("/erro", (req, res) => {
 
 app.get("/login", (req, res) => {
     res.render('pages/login');
+    return response.json(userLogin)
 });
 
 app.get("/noticias", (req, res) => {
