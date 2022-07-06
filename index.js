bb8c27034ead98da9723b04a4ac1e0b9547bff3e
Cloud <jvhc3002@gmail.com>
2022-07-06 18:08:19 -0300

    Cloud Commit

--- index.js
+++ index.js
@@ -1,53 +1,62 @@
-const { response } = require('express');
-const express = require('express');
-const app = express();
-const port = 3007;
+const { response } = require('express')
+const express = require('express')
+const app = express()
+const port = 3007
 
-app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"))
+app.set('view engine', 'ejs')
+app.use(express.static(__dirname + '/public'))
 
 const allNewUser = () => {
-let contentPreference= ["Tecnology", "Enem", "História Brasileira", "Bestsellers", "línguas"]
-let users= [
-    {usuario:"Ana Laura", idade: 18, topPreferences: contentPreference, grauEscolar: "Ensino Médio completo"}
-]
-console.log(users)
+    let contentPreference = [
+        'Tecnology',
+        'Enem',
+        'História Brasileira',
+        'Bestsellers',
+        'línguas'
+    ]
+    let users = [
+        {
+            usuario: 'Ana Laura',
+            idade: 18,
+            topPreferences: contentPreference,
+            grauEscolar: 'Ensino Médio completo'
+        }
+    ]
+    console.log(users)
 }
 
-function userLogin(){
-console.log[{usuario: "Gui", senha: "#PizzacomAmor"}]
-} 
+function userLogin() {
+    console.log[{ usuario: 'Gui', senha: '#PizzacomAmor' }]
+}
 
-app.get("/", (req, res) => {
-    res.render('pages/index');
-});
+app.get('/', (req, res) => {
+    res.render('pages/index')
+})
 
-app.get("/cadastro-login", (req, res) => {
-    res.render('pages/cadastro-login');
-}); 
+app.get('/cadastro-login', (req, res) => {
+    res.render('pages/cadastro-login')
+})
 
-app.get("/cadastro", (req, res) => {
-    res.render('pages/cadastro');
-    return response.json({allNewUser}) 
-});
+app.get('/cadastro', (req, res) => {
+    res.render('pages/cadastro')
+})
 
-app.get("/erro", (req, res) => {
-    res.render('pages/Erro');
-});
+app.get('/erro', (req, res) => {
+    res.render('pages/Erro')
+})
 
-app.get("/login", (req, res) => {
-    res.render('pages/login');
-    return response.json(userLogin)
-});
+app.get('/login', (req, res) => {
+    res.render('pages/login')
+})
 
-app.get("/noticias", (req, res) => {
-    res.render('pages/noticias');
-});
+app.get('/noticias', (req, res) => {
+    res.render('pages/noticias')
+})
 
-app.get("/usuario", (req, res) => {
-    res.render('pages/usuario');
-});
+app.get('/usuario', (req, res) => {
+    res.render('pages/usuario')
+})
 
-app.listen(port, () =>{
-     console.log(`projeto rodando na port: ${port}`);
-});
+app.listen(port, () => {
+    console.log(`projeto rodando na port: ${port}`)
+})
