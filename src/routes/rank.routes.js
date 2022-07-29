7f0e0725cdd92c9bd6e9b90207d0ea21f430bfd6
Walmir-Ag <walmirfaguiar100@gmail.com>
2022-07-29 17:38:13 -0300

    route

--- src/routes/rank.routes.js
+++ src/routes/rank.routes.js
@@ -1,10 +1,7 @@
 const express = require("express")
-
-const chamandoRankcontroller = require("../controllers")
+const chamandoRankcontroller = require("../controllers/")
 
 const rankRoute = express.Router();
-
-
 rankRoute.get.chamandoRankcontroller
 
 module.exports = rankRoute;
