21bb17b3d54dd5732cfa65c7dbf393cfdb1d4ae5
Walmir-Ag <walmirfaguiar100@gmail.com>
2022-09-13 14:38:01 -0300

    fix correcao

--- frontEnd/service/NewUserApi.js
+++ frontEnd/service/NewUserApi.js
@@ -21,7 +21,7 @@ async function pegar() {
 async function enviar() {
     const dados = document.getElementByClass(".input_cadastro");
     const valorDados = dados.value
-    valorDados = [] 
+    console.log(valorDados)
     const adress = await fetch(`http://localhost:4000/${valorDados}/criarUsu`)
     const jsonAdress = await adress.json()
     console.log(jsonAdress)
