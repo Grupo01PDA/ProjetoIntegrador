b9b2a6f075ba9873c7332f752c935fe0a68ec7f9
Walmir-Ag <walmirfaguiar100@gmail.com>
2022-09-13 17:44:02 -0300

    rascunho de pegar

--- frontEnd/service/NewUserApi.js
+++ frontEnd/service/NewUserApi.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-async function pegar() {
+/*async function pegar() {
     fetch("http://localhost:4000/pegarusuario")
         .then(res => res.json())
         .then(res => {
             const usuarios = res.usuarios;
-            const getId = document.querySelector(".input_cadastro");
-            dados = getId.value
+            const getId = document.querySelector(".input_apelido");
+            dados = getId[nome, apelido].value
             usuarios.map(usuario => {
                 const li = document.createElement("li")
                 li.innerHTML = usuario.nome
@@ -14,11 +14,12 @@ async function pegar() {
         }).catch(error => {
             console.log(error)
         })
-}
+}*/
 })
 
 const dados = document.getElementByClass(".input_cadastro");
 const valorDados = dados.value
+
 async function enviar() {
     valorDados
     console.log(valorDados)
