f43628caadac8b2ce3edec04d2308eac222d038a
Walmir-Ag <walmirfaguiar100@gmail.com>
2022-09-13 14:34:39 -0300

    corrigindo node modules

---
+++ frontEnd/service/NewUserApi.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function () {
+async function pegar() {
+    fetch("http://localhost:4000/pegarusuario")
+        .then(res => res.json())
+        .then(res => {
+            const usuarios = res.usuarios;
+            const getId = document.querySelector(".input_cadastro");
+            dados = getId.value
+            usuarios.map(usuario => {
+                const li = document.createElement("li")
+                li.innerHTML = usuario.nome
+                getId.appendChild(li)
+            })
+        }).catch(error => {
+            console.log(error)
+        })
+}
+})
+
+
+async function enviar() {
+    const dados = document.getElementByClass(".input_cadastro");
+    const valorDados = dados.value
+    valorDados = [] 
+    const adress = await fetch(`http://localhost:4000/${valorDados}/criarUsu`)
+    const jsonAdress = await adress.json()
+    console.log(jsonAdress)
+    for (var atributo in jsonAdress) {
+        atributo.innerHTML = jsonAdress[atributo];        
+    }
+}
