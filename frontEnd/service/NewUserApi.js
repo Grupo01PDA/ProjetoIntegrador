b74c4720fab4a67957135cacb08b5c59a1f8dc86
Walmir-Ag <walmirfaguiar100@gmail.com>
2022-09-16 14:25:38 -0300

    fix concertando codigo

--- frontEnd/service/NewUserApi.js
+++ frontEnd/service/NewUserApi.js
@@ -1,20 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-/*async function pegar() {
+async function pegar() {
     fetch("http://localhost:4000/pegarusuario")
         .then(res => res.json())
         .then(res => {
             const usuarios = res.usuarios;
             const getId = document.querySelector(".input_apelido");
             dados = getId[nome, apelido].value
-            usuarios.map(usuario => {
-                const li = document.createElement("li")
-                li.innerHTML = usuario.nome
-                getId.appendChild(li)
-            })
-        }).catch(error => {
-            console.log(error)
-        })
-}*/
+
 })
 const dados = document.getElementByClass(".input_cadastro");
 const valorDados = dados.value
@@ -22,7 +14,7 @@ const valorDados = dados.value
 async function enviar() {
     valorDados
     console.log(valorDados)
-    const adress = await fetch(`http://localhost:4000/${valorDados}/criarUsu`)
+    const adress = await fetch(`http://localhost:4000/criarUsu`)
     const jsonAdress = await adress.json()
     console.log(jsonAdress)
     for (var atributo in jsonAdress) {
