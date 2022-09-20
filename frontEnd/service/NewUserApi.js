c4c0278d018663d28a9d4ddcf6b939832c74d561
Walmir-Ag <walmirfaguiar100@gmail.com>
2022-09-20 09:31:36 -0300

    fetch

--- frontEnd/service/NewUserApi.js
+++ frontEnd/service/NewUserApi.js
@@ -1,13 +1,13 @@
-document.addEventListener("DOMContentLoaded", function () {
-async function pegar() {
-    fetch("http://localhost:4000/pegarusuario")
-        .then(res => res.json())
-        .then(res => {
-            const usuarios = res.usuarios;
-            const getId = document.querySelector(".input_apelido");
-            dados = getId[nome, apelido].value
+// document.addEventListener("DOMContentLoaded", function () {
+// async function pegar() {
+//     fetch("http://localhost:4000/pegarusuario")
+//         .then(res => res.json())
+//         .then(res => {
+//             const usuarios = res.usuarios;
+//             const getId = document.querySelector(".input_apelido");
+//             dados = getId[nome, apelido].value
 
-})
+// })
 const dados = document.getElementByClass(".input_cadastro");
 const valorDados = dados.value
 
