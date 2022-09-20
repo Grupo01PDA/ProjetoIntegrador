dc7a47c17a1b8851e43b704a1990001b8cd17205
Walmir-Ag <walmirfaguiar100@gmail.com>
2022-09-20 09:29:00 -0300

    fix -forçar criacao

--- backEnd/src/controllers/usuarios/criandoUsuarioController.js
+++ backEnd/src/controllers/usuarios/criandoUsuarioController.js
@@ -4,7 +4,7 @@ const criandoUsuarioController = async (req, res)=>{
   //verifica se a tabela existe no banco
   //caso não existe ele cria
   //se existe ele insere os dados na tabela
-  await db.sync()
+  await db.sync({force: true})
   const { apelido, nome, sobrenome, escolaridade, email, senha } = req.body
   const novoUsuario = await usuario.create({
       apelido,
