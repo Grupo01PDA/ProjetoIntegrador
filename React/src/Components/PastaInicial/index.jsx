7aae6c335c626574f35e68b1bd3fed04d5c0faf8
Cloud <jvhc3002@gmail.com>
2022-10-12 18:17:04 -0300

    feat: Página inicial em react

--- React/src/Components/PastaInicial/index.jsx
+++ React/src/Components/PastaInicial/index.jsx
@@ -1,12 +1,21 @@
 import Header from "../Header";
+import Footer from "../Footer"
 import BarraLateral from "../BarraLateral";
+import Empty from "../Empty"
+import "./index.css";
 
 function PaginaInicial(){
   return(
-    <main>
+    <div>
       <Header />
-      <BarraLateral />
-    </main>
+      <main>
+        <BarraLateral />
+        <div className="feed">
+          <Empty />
+        </div>
+      </main>
+      <Footer />
+    </div>
   )
 }
 
