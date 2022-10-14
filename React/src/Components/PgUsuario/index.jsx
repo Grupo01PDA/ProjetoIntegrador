c8a0ae3f24516a867e9f6e442c28dfd68bfabe82
Walmir-Ag <walmirfaguiar100@gmail.com>
2022-10-14 14:09:22 -0300

    pagina usuario react

--- React/src/Components/PgUsuario/index.jsx
+++ React/src/Components/PgUsuario/index.jsx
@@ -1,9 +1,11 @@
 import Header from "../Header"
+import PostsUsuarios_Feed from "./PostsUsuario_Feed";
 
 function PgUsuario() {
     return(
        <div>
        <Header/>
+       <PostsUsuarios_Feed/>
        </div>
     );
 }
