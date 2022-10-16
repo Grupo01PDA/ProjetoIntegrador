51332ba1e55edce6acd04f4b4b8e222b18403823
Walmir-Ag <walmirfaguiar100@gmail.com>
2022-10-16 19:51:49 -0300

    pagina de usuario

---
+++ React/src/Components/Empty/index.jsx
@@ -0,0 +1,14 @@
+import "./index.css"
+
+function Empty(){
+    return(
+        <section class="empty">
+  <h2>Desculpe o incomodo!</h2>
+
+  <h3>Estamos procurando por seus posts</h3>
+  
+</section>
+        )
+}
+
+export default Empty
--- React/src/Components/PgUsuario/index.jsx
+++ React/src/Components/PgUsuario/index.jsx
@@ -1,14 +1,18 @@
-import Header from "../Header"
-import PostsUsuarios_Feed from "./PostsUsuario_Feed";
-import EditPErfil from "../PgUsuario/EditPErfil"
-
-
+import Header from "../Header";
+import EditPErfil from "../PgUsuario/EditPErfil";
+import Usuarios_Feed from "./Usuario_Feed/index";
+import Empty from "../Empty";
+import Footer from "../Footer";
+import { Fragment } from "react";
 
 function PgUsuario() { 
     return(
        <div> 
     <Header/>
     <EditPErfil/>
+    <Usuarios_Feed/>
+    <Empty/>
+    <Footer/>
        </div>
 
     );
