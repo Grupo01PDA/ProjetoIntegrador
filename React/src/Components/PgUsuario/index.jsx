fd7b73c78c79d94568f6b0db4e2ad4836f5b3480
limana-1247 <analauramasca@gmail.com>
2022-10-16 19:49:40 -0300

    Alterações de Laura

--- React/src/Components/PgUsuario/index.jsx
+++ React/src/Components/PgUsuario/index.jsx
@@ -1,7 +1,8 @@
-import Header from "../Header"
-import PostsUsuarios_Feed from "./PostsUsuario_Feed";
-import EditPErfil from "../PgUsuario/EditPErfil"
-
+import Header from "../Header";
+import EditPErfil from "../PgUsuario/EditPErfil";
+import Usuarios_Feed from "./PostsUsuario_Feed/index";
+import Empty from "../Empty"
+import Footer from "./Footer"
 
 
 function PgUsuario() { 
@@ -9,8 +10,11 @@ function PgUsuario() {
        <div> 
     <Header/>
     <EditPErfil/>
-       </div>
+    <Usuarios_Feed/>
+    <Empty/>
+    <Footer/>
+</div>
 
-    );
+);
 }
 export default PgUsuario;
