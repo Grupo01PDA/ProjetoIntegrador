cb9909f420ab9196c0bfd7f42487a706f4381ad4
andrew <aklayver@gmail.com>
2022-10-16 15:02:59 -0300

    tela de login

--- React/src/App.js
+++ React/src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Textos from './Components/Login/Textos/Index';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          
-          aaaaaasdkfasodfkasrjwoqeklnfmlsdkjfnaklsdmnclkasdjnfoasdjlnfvadjklsnfz
-          Learn React
-        </a>
-      </header>
+    <Textos />
+     
     </div>
   );
 }
