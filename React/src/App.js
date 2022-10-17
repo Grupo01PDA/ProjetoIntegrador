1ab7947bb38538d485005328fcb021c531569c43
wandersoncbjr <wandersoncbjr2@gmail.com>
2022-10-17 14:46:03 -0300

    criando rotas

--- React/src/App.js
+++ React/src/App.js
@@ -1,12 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import './App.css'
+import Cadastro from './pages/cadastro'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+
+       
       </Routes>
     </Router>
 
