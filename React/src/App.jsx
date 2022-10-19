d1f728f033dc4b99f79ac69fe69a4ee15e590265
Cloud <jvhc3002@gmail.com>
2022-10-19 16:30:51 -0300

    Feat: botão voltar página cadastro

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import './App.css'
-import Cadastro from './pages/cadastro'
+import Cadastro from './pages/Cadastro'
 
 
 
