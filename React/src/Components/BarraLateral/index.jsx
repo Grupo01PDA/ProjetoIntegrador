d4b64f2a9bea1b5c6e395a8d9e6e79f3758d53f6
limana-1247 <analauramasca@gmail.com>
2022-10-19 15:30:29 -0300

    Alterações de Laura

--- React/src/Components/BarraLateral/index.jsx
+++ React/src/Components/BarraLateral/index.jsx
@@ -6,7 +6,7 @@ function BarraLateral(){
       <ul class="lista">li
         <li><a href="/">Perfil</a></li>
         <li><a href="/">Crie seu Cronograma</a></li>
-        <li><a href="/">Grupos</a></li>
+        <li><a href="https://discord.com/channels/1032357491792687145/1032357492413440111">Grupos</a></li>
         <li><a href="/">Provas anteriores</a></li>
         <li><a href="/">Simulados</a></li>
         <li><a href="/">Gabaritos</a></li>
