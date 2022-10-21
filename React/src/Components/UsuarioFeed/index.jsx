9f5380fb0aaaaa1d0f49b0175805e6d8d6adda77
Cloud <jvhc3002@gmail.com>
2022-10-21 20:36:11 -0300

    Feat: Página de usuario funcionando (frontend)

--- React/src/Components/UsuarioFeed/index.jsx
+++ React/src/Components/UsuarioFeed/index.jsx
@@ -1,11 +1,9 @@
 import "./index.css";
 
-function Usuarios_Feed(){
+function UsuariosFeed(){
     return(
-            <nav class="posts_etc" style={{ 
-                backgroundImage: `url("")` 
-              }}>
-            <button id="posts" class="postagens">
+        <nav class="posts_etc">
+            <button id="posts" class="postagens btnActive">
                 <svg class="posts-svg" width="34" height="34" viewBox="0 0 34 34" fill="none"
                     xmlns="http://www.w3.org/2000/svg">
                     <rect x="0.5" y="0.5" width="14" height="14" stroke="#0F0F0F" />
@@ -13,7 +11,7 @@ function Usuarios_Feed(){
                     <rect x="0.5" y="19.5" width="14" height="14" stroke="#0F0F0F" />
                     <rect x="19.5" y="19.5" width="14" height="14" stroke="#0F0F0F" />
                 </svg>
-                <span>Meus posts</span>
+                <span className="userInputs">Meus posts</span>
             </button>
             <button id="salvos" class="salvos">
                 <svg class="salvos-svg" width="28" height="36" viewBox="0 0 28 36" fill="none"
@@ -25,7 +23,7 @@ function Usuarios_Feed(){
                     <path d="M27.2563 35L27.2563 1.00034" stroke="#0F0F0F" stroke-linecap="round" />
                     <path d="M27.2563 34.9996L14.2208 21.9641" stroke="#0F0F0F" stroke-linecap="round" />
                 </svg>
-                <span>Salvos</span>
+                <span className="userInputs">Salvos</span>
             </button>
             <button id="provas" class="gabaritos">
                 <svg class="provas-svg" width="23" height="34" viewBox="0 0 23 34" fill="none"
@@ -39,10 +37,10 @@ function Usuarios_Feed(){
                     <line x1="2.6792" y1="25.0515" x2="20.1943" y2="25.0515" stroke="#0F0F0F" />
                     <line x1="2.6792" y1="29.1727" x2="20.1943" y2="29.1727" stroke="#0F0F0F" />
                 </svg>
-                <span>Provas e Gabaritos</span>
+                <span className="userInputs">Provas e Gabaritos</span>
             </button>
         </nav>
     )
 }
 
-export default Usuarios_Feed;
+export default UsuariosFeed;
