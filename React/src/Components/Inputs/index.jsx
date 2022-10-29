dbf649ae0b5eaa196259834a20217c94efcc6a59
Walmir-Ag <walmirfaguiar100@gmail.com>
2022-10-29 12:15:58 -0300

    correção de bugs

--- React/src/Components/Inputs/index.jsx
+++ React/src/Components/Inputs/index.jsx
@@ -30,74 +30,54 @@ function Inputs() {
     }
 
     return (
-        <div className='cadastro'>
-            <h2>CADDASTRE-SE</h2>
-            <form className="form" onSubmit={cadastrando}>
-                <div className="cadastro_div">
-                    <label for="apelido">Seu apelido</label>
-                    <input className="input_cadastro" type="text" name="apelido" id="apelido"
-                        placeholder="Digite seu apelido..." onChange={testeNome} ref={apelidoRef}/>
-                 </div>       
         <div className='caixaInputs'>
-            <h2>CADASTRE-SE</h2>
-            <div className="cadastro" action="/cadastro" method="POST">
-                <div className="inputs_div">
-                    <label for="apelido">Seu apelido</label>
-                    <input className="input_inputs" type="text" name="apelido" id="apelido"
-                        placeholder="Digite seu apelido..." />
-                </div>
-                <div className="inputs_div">
-                    <label for="nome">Seu Nome</label>
-                    <input className="input_cadastro" type="text" name="nome" id="nome" placeholder="Digite seu nome..." ref={nomeRef}/>
-                    <input className="input_inputs" type="text" name="nome" id="nome" placeholder="Digite seu nome..." />
-                </div>
-                <div className="inputs_div">
-                    <label for="sobrenome">Seu Sobrenome</label>
-                    <input className="input_cadastro" type="text" name="sobrenome" id="sobrenome"
-                        placeholder="Digite seu sobrenome..." ref={sobrenomeRef}/>
-                    <input className="input_inputs" type="text" name="sobrenome" id="sobrenome"
-                        placeholder="Digite seu sobrenome..." />
-                </div>
+        <h2>CADASTRE-SE</h2>
+        <form className="cadastro" action="/cadastro" method="POST">
+            <div className="inputs_div">
+                <label for="apelido">Seu apelido</label>
+                <input className="input_inputs" type="text" name="apelido" id="apelido"
+                    placeholder="Digite seu apelido..." />
+            </div>
+            <div className="inputs_div">
+                <label for="nome">Seu Nome</label>
+                <input className="input_inputs" type="text" name="nome" id="nome" placeholder="Digite seu nome..." />
+            </div>
+            <div className="inputs_div">
+                <label for="sobrenome">Seu Sobrenome</label>
+                <input className="input_inputs" type="text" name="sobrenome" id="sobrenome"
+                    placeholder="Digite seu sobrenome..." />
+            </div>
 
-                <div className="inputs_div">
-                    <label for="escolaridade">Grau de escolaridade</label>
-                    <select className="escolaridade" name="escolaridade" id="escolaridade" ref={escolaridadeRef}>
-                        <option value="default">Selecione a opção em que você se encaixa</option>
-                        <option value="1">Não conclui o ensino fundamental</option>
-                        <option value="2">Conclui o ensino fundamental, não conclui o ensino médio</option>
-                        <option value="3">Conclui o ensino médio, não tenho ensino superior</option>
-                        <option value="4">Tenho ensino superior</option>
-                    </select>
-                </div>
+            <div className="inputs_div">
+                <label for="escolaridade">Grau de escolaridade</label>
+                <select className="escolaridade" name="escolaridade" id="escolaridade">
+                    <option value="default">Selecione a opção em que você se encaixa</option>
+                    <option value="1">Não conclui o ensino fundamental</option>
+                    <option value="2">Conclui o ensino fundamental, não conclui o ensino médio</option>
+                    <option value="3">Conclui o ensino médio, não tenho ensino superior</option>
+                    <option value="4">Tenho ensino superior</option>
+                </select>
+            </div>
 
-                <div className="inputs_div">
-                    <label for="email">Seu email</label>
-                    <input className="input_cadastro" type="email" name="email" id="email"
-                        placeholder="Digite seu email..." ref={emailRef} />
-                    <input className="input_inputs" type="email" name="email" id="email"
-                        placeholder="Digite seu email..." />
-                </div>
-                <div className="inputs_div">
-                    <label for="senha">Sua senha</label>
-                    <input className="input_cadastro" type="password" name="senha" id="senha"
-                        placeholder="Digite sua senha..." ref={senhaRef} />
-                </div>
+            <div className="inputs_div">
+                <label for="email">Seu email</label>
+                <input className="input_inputs" type="email" name="email" id="email"
+                    placeholder="Digite seu email..." />
+            </div>
+            <div className="inputs_div">
+                <label for="senha">Sua senha</label>
+                <input className="input_inputs" type="password" name="senha" id="senha"
+                    placeholder="Digite sua senha..." />
+            </div>
 
-                <div className="enviar-cadastro">
-                    <button className="button" type="submit" >Cadastrar</button>
-                    <input className="input_inputs" type="password" name="senha" id="senha"
-                        placeholder="Digite sua senha..." />
-                </div>
-
-                <div className="enviar-inputs">
-                    <button className="button" type="submit">Cadastrar</button>
-                    <div class="link-login">
-                        <br />Já tem uma conta?
-                        <a href="/login">Acesse</a>
-                    </div>
+            <div className="enviar-inputs">
+                <button className="button" type="submit">Cadastrar</button>
+                <div class="link-login">
+                    <br />Já tem uma conta?
+                    <a href="/login">Acesse</a>
                 </div>
             </div>
-        </div>
+
         </form>
     </div>
     )
