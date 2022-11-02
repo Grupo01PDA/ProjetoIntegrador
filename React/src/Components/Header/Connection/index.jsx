5fc8fa8a3a48307cd0511d0cca09d4d2704e0329
wandersoncbjr <wandersoncbjr2@gmail.com>
2022-11-01 21:45:52 -0300

    feat: modo dark

--- React/src/Components/Header/Connection/index.jsx
+++ React/src/Components/Header/Connection/index.jsx
@@ -1,13 +1,71 @@
 import "./index.css";
+import { useState } from "react";
 
 function Connection() {
-  return(
+
+
+  function modoDark() {
+
+    function brancoPreto(tema) {
+      document.documentElement.style.setProperty('--preto-base', tema)
+    }
+
+    brancoPreto('#f0f0f0');
+
+    function brancoBase(tema) {
+      document.documentElement.style.setProperty('--branco-base', tema)
+    }
+    brancoBase('#0f0f0f');
+
+    function brancoFundo(tema) {
+      document.documentElement.style.setProperty('--branco-de-fundo', tema)
+    }
+    brancoFundo('#1b1b1b');
+
+
+  }
+
+  function modoWhite() {
+    function setColo(tema) {
+      document.documentElement.style.setProperty('--preto-base', tema)
+    }
+
+    setColo('#0f0f0f');
+
+    function brancoBase(tema) {
+      document.documentElement.style.setProperty('--branco-base', tema)
+    }
+    brancoBase('#f0f0f0');
+
+    function fundoBranco(tema) {
+      document.documentElement.style.setProperty('--branco-de-fundo', tema)
+    }
+    fundoBranco('#e6e6e6');
+  }
+
+  const [count, setCount] = useState(0)
+
+  console.log(count)
+
+
+  function mudarCor() {
+
+    if (count === 0) {
+      modoDark();
+      setCount(count + 1)
+    }
+    else {
+      modoWhite();
+      setCount(0)
+    }
+  }
+  return (
     <div className="links">
       <a className="link" href="/entrar">Entre</a>
 
       <input className="input-check" type="checkbox" id="checkbox" />
       <label class="checkbox" for="checkbox">
-        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-circle-half" viewBox="0 0 16 16">
+        <svg onClick={() => mudarCor()} xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-circle-half" viewBox="0 0 16 16">
           <path d="M8 15A7 7 0 1 0 8 1v14zm0 1A8 8 0 1 1 8 0a8 8 0 0 1 0 16z" />
         </svg>
       </label>
--- React/src/Components/HeaderEntrar/index.jsx
+++ React/src/Components/HeaderEntrar/index.jsx
@@ -1,4 +1,6 @@
 import "./index.css";
+import Connectio from "../Header/Connection";
+
 
 
 function HeaderEntrar() {
@@ -11,14 +13,7 @@ function HeaderEntrar() {
 
         <a className="cadastrar-botao" href="/cadastro">Cadastre-se</a>
         <a className="login-botao" href="/login">Conecte-se</a>
-
-
-        <input className="input-check" type="checkbox" id="checkbox" />
-        <label class="checkbox" for="checkbox">
-          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-circle-half" viewBox="0 0 16 16">
-            <path d="M8 15A7 7 0 1 0 8 1v14zm0 1A8 8 0 1 1 8 0a8 8 0 0 1 0 16z" />
-          </svg>
-        </label>
+        <Connectio />
       </div>
     </div>
   )
