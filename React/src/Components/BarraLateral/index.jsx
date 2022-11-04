7c7c78b8aa4bab985bcacf474a3aceb584163b4a
Cloud <jvhc3002@gmail.com>
2022-11-04 19:01:12 -0300

    alterando target link disc

--- React/src/Components/BarraLateral/index.jsx
+++ React/src/Components/BarraLateral/index.jsx
@@ -6,7 +6,7 @@ function BarraLateral(){
       <ul class="lista">
         <li><a href="/usuario">Perfil</a></li>
         <li><a href="/">Crie seu Cronograma</a></li>
-        <li><a href="https://discord.com/channels/1032357491792687145/1032357492413440111">Grupos</a></li>
+        <li><a target="_blank" href="https://discord.com/channels/1032357491792687145/1032357492413440111">Grupos</a></li>
         {/* <li><a href="/">Provas anteriores</a></li>
         <li><a href="/">Simulados</a></li>
         <li><a href="/">Gabaritos</a></li>
