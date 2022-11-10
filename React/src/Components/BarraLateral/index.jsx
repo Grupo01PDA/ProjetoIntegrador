0ddd086450c1f7c01ef769ebd4158dc814af1162
wandersoncbjr <wandersoncbjr2@gmail.com>
2022-11-10 17:32:25 -0300

    fix

--- React/src/Components/BarraLateral/index.jsx
+++ React/src/Components/BarraLateral/index.jsx
@@ -3,15 +3,11 @@ import "./index.css";
 function BarraLateral(){
   return(
     <aside class="barra-lateral">
-      <ul class="lista">
+      <div class="lista">
         <li><a href="/usuario">Perfil</a></li>
         <li><a href="/">Comunidade</a></li>
         <li><a target="_blank" rel="noreferrer" href="https://discord.com/channels/1032357491792687145/1032357492413440111">Grupos</a></li>
-        {/* <li><a href="/">Provas anteriores</a></li>
-        <li><a href="/">Simulados</a></li>
-        <li><a href="/">Gabaritos</a></li>
-        <li><a href="/">Encontre um Professor</a></li> */}
-      </ul>
+      </div>
     </aside>
   )
 }
